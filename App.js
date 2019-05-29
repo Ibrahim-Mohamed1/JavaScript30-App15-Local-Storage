dd24d4c7b1dc10b5190ce4d33b231c9ebe45e661
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-29 13:10:40 -0600

    update

---
+++ App.js
@@ -0,0 +1,12 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+        text,
+        done: false
+    };
+}
