266b8a493cf4ed27cdf311999f3e0873a6330fe3
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-29 13:11:30 -0600

    update

--- App.js
+++ App.js
@@ -25,3 +25,11 @@ function populateList(plates = [], platesList) {
       `;
     }).join('');
 }
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip this unless it's an input
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
